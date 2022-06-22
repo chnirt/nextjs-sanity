fb5a9147091aeed0b6283c731e4f47400d540377
chnirt <trinhchinchin@gmail.com>
2022-06-22 14:15:22 +0700

    Update: combine 2 ports

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,4 @@
 /** @type {import('tailwindcss').Config} */
-const STUDIO_REWRITE = {
-  source: '/studio/:path*',
-  destination:
-    process.env.NODE_ENV === 'development'
-      ? 'http://localhost:3333/studio/:path*'
-      : '/studio/index.html',
-}
 module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
@@ -15,5 +8,4 @@ module.exports = {
     extend: {},
   },
   plugins: [require('@tailwindcss/aspect-ratio')],
-  rewrites: () => [STUDIO_REWRITE],
 }
